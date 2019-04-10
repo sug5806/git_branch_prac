252daff1a831c0cedd622819f9cbeb4c48833833
sug5806 <sug5806@gmail.com>
2019-04-10 11:21:37 +0900

    feat: add conditional statement Add if statement to print while i is even. if i is odd, print 'Nothing to say'

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-for _ in range(1,5+1):
-    print("hello python with git in another dimension.")
+for i in range(1,5+1):
+    if not i % 2:
+        print("hello python with git in another dimension.")
+    else:
+        print("Nothing to say")
