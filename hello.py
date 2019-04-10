b796707d966c329ba196b91193488447b67b0259
sug5806 <sug5806@gmail.com>
2019-04-10 11:51:01 +0900

    feat: functionalize hello.py while __name__ is equal to __main, do print_hello().

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-for i in range(1,5+1):
-    if not i % 2:
-        print("hello python with git in another dimension.")
-    else:
-        print("Nothing to say")
+def print_hello():
+    for i in range(1,5+1):
+        if not i % 2:
+            print("hello python with git in another dimension.")
+        else:
+            print("Nothing to say")
+
+
+if __name__=="__main__":
+    print_hello()
+
